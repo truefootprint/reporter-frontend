fcbb57afa2f64621e36a07bab94c97f7cf29ab2e
Chris Patuzzo <chris@patuzzo.co.uk>
2019-09-16 16:54:26 +0100

    Use ids instead of text for question radio inputs

--- components/question/Question.jsx
+++ components/question/Question.jsx
@@ -2,21 +2,24 @@ import { useContext } from "react";
 import ActionContext from "../../contexts/action_context";
 import AnswerQuestion from "../../actions/answer_question";
 
-const Question = ({ text }) => {
+const Question = ({ id, text }) => {
   let context = useContext(ActionContext);
 
   const handleChange = (event) => {
     let { name, value } = event.target;
 
-    context.process(new AnswerQuestion(name, value));
+    let questionId = parseInt(name, 10);
+    let action = new AnswerQuestion(questionId, value);
+
+    context.process(action);
   };
 
   return (
     <div className="question">
       <span>{text}</span>
 
-      <input type="radio" name={text} value="yes" onChange={handleChange} /> Yes
-      <input type="radio" name={text} value="no" onChange={handleChange} /> No
+      <input type="radio" name={id} value="yes" onChange={handleChange} /> Yes
+      <input type="radio" name={id} value="no" onChange={handleChange} /> No
     </div>
   );
 };
