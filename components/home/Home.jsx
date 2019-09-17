3963cf5f1a24622dc05e35714c0a59ab0201bad2
Chris Patuzzo <chris@patuzzo.co.uk>
2019-09-17 10:49:14 +0100

    Add a Submit button that sends responses to the backend

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,11 +1,14 @@
 import { useState, useContext } from "react";
 import data from "../../data.json";
 import MainContext from "../../contexts/main_context";
+import Client from "../../helpers/client";
 import Activity from "../activity";
+import Submit from "../submit";
 
 const Home = () => {
   let [appState, setAppState] = useState(data);
   let [actionLog, setActionLog] = useState([]);
+  let client = new Client();
 
   let process = (action) => {
     setActionLog(l => l.concat(action));
@@ -13,12 +16,13 @@ const Home = () => {
   };
 
   let contextValue = {
-    appState, setAppState, actionLog, setActionLog, process
+    appState, setAppState, actionLog, setActionLog, process, client,
   };
 
   return (
     <MainContext.Provider value={contextValue}>
       {appState.activities.map((props, i) => <Activity key={i} {...props} />)}
+      <Submit />
     </MainContext.Provider>
   );
 };
---
+++ components/submit/Submit.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import MainContext from "../../contexts/main_context";
+
+const Submit = () => {
+  const { client, actionLog, setActionLog } = useContext(MainContext);
+
+  const handleSubmit = () => {
+    client.submitResponses(actionLog);
+    setActionLog([]);
+  };
+
+  return (
+    <div className="submit">
+      <br/>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
+
+export default Submit;
